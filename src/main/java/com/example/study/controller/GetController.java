683ee226695a7100b58db0b43b75a6ea200c03a6
NamCheolKim <pkeowkd@naver.com>
2021-01-16 02:55:32 +0900

    spring_example;

--- src/main/java/com/example/study/controller/GetController.java
+++ src/main/java/com/example/study/controller/GetController.java
@@ -1,7 +1,6 @@
 package com.example.study.controller;
 
 import com.example.study.model.SearchParam;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
