ace791d2b4d064edacbca4db42a7a72ac541ec87
NamCheolKim <pkeowkd@naver.com>
2021-01-28 02:52:05 +0900

    spring_example;

--- src/test/java/com/example/study/repository/AdminUserRepositoryTest.java
+++ src/test/java/com/example/study/repository/AdminUserRepositoryTest.java
@@ -6,8 +6,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import java.time.LocalDateTime;
-
 public class AdminUserRepositoryTest extends StudyApplicationTests {
 
     @Autowired
